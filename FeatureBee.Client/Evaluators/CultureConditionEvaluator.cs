5cab9bbce157cb801361af9049a4c859a60206f3
Philipp Garbe <pgarbe@autoscout24.com>
2013-12-18 07:04:27 +0100

    New Evaluators

---
+++ FeatureBee.Client/Evaluators/CultureConditionEvaluator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace FeatureBee.Evaluators
+{
+    internal class CultureConditionEvaluator : IConditionEvaluator
+    {
+        public string Name { get { return "culture"; } }
+        public bool IsFulfilled(string[] values)
+        {
+            var currentCulture = Thread.CurrentThread.CurrentCulture.Name;
+            return values.Any(x => x.Equals(currentCulture, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
