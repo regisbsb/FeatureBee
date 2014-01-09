9aef8072b4c3e0e9754caaff357015cf6aded9b4
Philipp Garbe <pgarbe@autoscout24.com>
2014-01-09 13:18:57 +0100

    updated r# settings

--- FeatureBee.Server/Domain/Models/FeatureReleasedWithConditionsEvent.cs
+++ FeatureBee.Server/Domain/Models/FeatureReleasedWithConditionsEvent.cs
@@ -4,7 +4,6 @@
 
     using FeatureBee.Server.Domain.Infrastruture;
 
-    // TODO: Find a better name
     public class FeatureReleasedWithConditionsEvent : IDomainEvent
     {
         public Guid AggregateId { get; set; }
