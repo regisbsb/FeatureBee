f4684d7cff6a0cbb99f76b0a326fe17b7b14ea85
Philipp Garbe <pgarbe@autoscout24.com>
2014-01-21 07:11:59 +0100

    Removed unused stuff

--- FeatureBee.Server/Models/ConditionViewModel.cs
+++ FeatureBee.Server/Models/ConditionViewModel.cs
@@ -16,19 +16,5 @@
         public string Type { get; set; }
 
         public virtual PersistableStringCollection Values { get; set; }
-        // public virtual List<ConditionValueViewModel> Values { get; set; }
-
-        //public void AddValue(string value)
-        //{
-        //    var model = new ConditionValueViewModel(value);
-        //    if (Values.Any(v => v.Value == model.Value)) return;
-        //    Values.Add(model);
-        //}
-
-        //public void RemoveValue(string value)
-        //{
-        //    if (Values.All(v => v.Value != value)) return;
-        //    Values.Remove(Values.Single(_ => _.Value == value));
-        //}
     }
 }
