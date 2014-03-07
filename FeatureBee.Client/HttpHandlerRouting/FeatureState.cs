6fc65e223b61050e6bd0aa33a63e1a1fba2e0abb
MatthiasKainer <mail@matthias-kainer.de>
2014-03-07 07:24:42 +0100

    Added name of the feature to the result for featurestate and fixed the failing test

--- FeatureBee.Client/HttpHandlerRouting/FeatureState.cs
+++ FeatureBee.Client/HttpHandlerRouting/FeatureState.cs
@@ -18,12 +18,12 @@
             string featureName = context.Request.QueryString["name"];
             if (string.IsNullOrEmpty(featureName))
             {
-                context.Response.Write(serializer.Serialize(new { success = false, message = "the query parameter for name is missing" }));
+                context.Response.Write(serializer.Serialize(new { success = false, name = featureName, message = "the query parameter for name is missing" }));
             }
             else
             {
                 var isEnabled = Feature.IsEnabled(featureName);
-                context.Response.Write(serializer.Serialize(new { success = true, isEnabled }));
+                context.Response.Write(serializer.Serialize(new { success = true, name = featureName, isEnabled }));
             }
 
             context.Response.ContentType = "application/json";
