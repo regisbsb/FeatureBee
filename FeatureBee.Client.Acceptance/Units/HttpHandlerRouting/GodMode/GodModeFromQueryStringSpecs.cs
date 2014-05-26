cc9a0961061b97edd4dca94ed60d28738ff39508
Raphael Wiegand <raphael@stamail.de>
2014-05-26 16:38:06 +0200

    Added GodMode by QueryString Resolver

---
+++ FeatureBee.Client.Acceptance/Units/HttpHandlerRouting/GodMode/GodModeFromQueryStringSpecs.cs
@@ -0,0 +1,61 @@
+﻿using FeatureBee.GodMode;
+using FeatureBee.WireUp;
+using FluentAssertions;
+using Machine.Fakes;
+using Machine.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FeatureBee.Acceptance.Units.HttpHandlerRouting.GodMode
+{
+
+    public class GodModeFromQueryStringSpecs : WithSubject<GetGodModeFeaturesFromQueryString>
+    {
+        static HttpRequestBase requestBase;
+        static GodModeFeatureCollection result;
+
+        Establish context = () =>
+        {
+            The<GodModeFeatureStateEvaluator>()
+                .WhenToldTo(_ => _.Parse(Param<string>.IsAnything))
+                .Return(new GodModeFeatureCollection());
+            requestBase = An<HttpRequestBase>();
+            requestBase.WhenToldTo(_ => _.QueryString).Return((NameValueCollection)null);
+        };
+
+        public class When_getting_god_mode_features_but_no_header_value_set
+        {
+            Because of = () => result = Subject.GetGodModeFeatures(requestBase);
+
+            It should_return_no_result = () => result.Should().BeEmpty();
+        }
+
+        public class When_getting_god_mode_feature_with_a_featureBee_Header_and_value
+        {
+            Establish context = () =>
+            {
+                var collection = new GodModeFeatureCollection();
+                collection.Add("feature", true);
+
+                var header = new NameValueCollection();
+                header.Add("FeatureBee", "blablablub");
+
+                The<GodModeFeatureStateEvaluator>()
+                    .WhenToldTo(_ => _.Parse("blablablub"))
+                    .Return(collection);
+                requestBase.WhenToldTo(_ => _.QueryString).Return((NameValueCollection)header);
+            };
+
+            Because of = () => result = Subject.GetGodModeFeatures(requestBase);
+
+            It should_return_one_result = () => result.Count.Should().Be(1);
+
+            It should_have_parsed_the_value = () => The<GodModeFeatureStateEvaluator>().WasToldTo(_ => _.Parse("blablablub")).OnlyOnce();
+        }
+    }
+}
